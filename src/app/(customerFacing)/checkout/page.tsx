9015cc9118b59c04d98d552525219bca176b5cb4
Chris Mayes <Chris@mayes.me>
2024-06-06 15:12:22 +0100

    Update checkout styling

--- src/app/(customerFacing)/checkout/page.tsx
+++ src/app/(customerFacing)/checkout/page.tsx
@@ -4,8 +4,10 @@ import { useEffect, useState } from "react";
 import { useCart } from "@/context/CartContext";
 import { useRouter } from "next/navigation";
 import { CheckoutForm } from "../products/[id]/purchase/_components/CheckoutForm";
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 export default function CheckoutPage() {
     const { cart } = useCart();
@@ -54,14 +56,15 @@ export default function CheckoutPage() {
 
     return (
         <div className="max-w-5xl w-full mx-auto space-y-8">
-            <Button onClick={() => router.push("/products")} className="mb-4">
-                Return to Store
-            </Button>
+            <Link href="/products" className="flex items-center text-black">
+                <FontAwesomeIcon icon={faArrowLeft} className="mr-2 w-6 h-6" />
+                <span className="align-middle">Back to Products</span>
+            </Link>
             <div className="space-y-4">
                 {cart.map((item, index) => (
                     <div
                         key={index}
-                        className="flex gap-5 pt-8 pb-8 items-center"
+                        className="flex gap-5 pt-1 pb-1 items-center"
                     >
                         <div className="relative w-1/4 h-56">
                             <Image
