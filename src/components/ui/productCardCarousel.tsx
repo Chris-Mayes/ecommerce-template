6a1c23c9293c537bfc146e523342776a51c2a119
Chris Mayes <Chris@mayes.me>
2024-06-10 21:03:51 +0100

    Change product card carousel animation

--- src/components/ui/productCardCarousel.tsx
+++ src/components/ui/productCardCarousel.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 
 type CarouselProps = {
@@ -8,18 +8,35 @@ type CarouselProps = {
 
 const Carousel = ({ images }: CarouselProps) => {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+    const startCarousel = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+        intervalRef.current = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 3000);
+    };
+
+    const stopCarousel = () => {
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+            intervalRef.current = null;
+        }
+        setCurrentIndex(0);
+    };
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 5000);
-
-        return () => clearInterval(interval);
-    }, [images.length]);
+        return () => stopCarousel();
+    }, []);
 
     if (!images || images.length === 0) return null;
+
     return (
-        <div className="relative w-full h-full">
+        <div
+            className="relative w-full h-full"
+            onMouseEnter={startCarousel}
+            onMouseLeave={stopCarousel}
+        >
             {images.map((image, index) => (
                 <div
                     key={index}
